e4eb234f2e334a1b936230171beba4807b688ead
Eehoyoung <c65621@naver.com>
2025-08-09 01:05:57 +0900

    types: 글로벌 타입 선언
    Signed-off-by: Eehoyoung <c65621@naver.com>

---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+/// <reference types="react-native" />
+
+// This file contains type declarations for global JSX namespace
+// This is needed to fix the "TS2503: Cannot find namespace 'JSX'" error
---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+    interface Timeout {
+        _idleTimeout: number;
+        _idlePrev: any;
+        _idleNext: any;
+        _idleStart: number;
+        _onTimeout: () => void;
+        _timerArgs: any[];
+        _repeat: Function | null;
+    }
+}
