6c234048ba8bb193257a5a4ac447b5d3c8214f1c
Eehoyoung <c65621@naver.com>
2025-08-09 00:53:20 +0900

    config: Jest 테스트 환경 설정
    Signed-off-by: Eehoyoung <c65621@naver.com>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  preset: 'react-native',
+    preset: 'react-native',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testMatch: [
+        '**/__tests__/**/*.(ts|tsx|js)',
+        '**/*.(test|spec).(ts|tsx|js)'
+    ],
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/*.stories.{ts,tsx}'
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70
+        }
+    },
+    moduleNameMapping: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    }
 };
