e96b3bab69038163da3a19e19d473138d4718acd
Eehoyoung <c65621@naver.com>
2025-08-09 00:51:22 +0900

    config: ESLint 설정 추가
    Signed-off-by: Eehoyoung <c65621@naver.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    extends: [
+        '@react-native',
+        '@typescript-eslint/recommended',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-native/no-unused-styles': 'error'
+    }
 };
